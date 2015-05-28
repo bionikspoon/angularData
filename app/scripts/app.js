79b5054aee4a145b9c0dc9e98d7f34e96c682201
Manu <bionikspoon@gmail.com>
2015-05-28 00:45:32 -0500

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularDataApp
+ * @description
+ * # angularDataApp
+ *
+ * Main module of the application.
+ */
+angular.module('angularDataApp', [
+  'ngAnimate',
+  'ngAria',
+  'ngCookies',
+  'ngMessages',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch'
+])
+
+  .config(function ($routeProvider) {
+    $routeProvider
+
+      .when('/', {
+        templateUrl: 'views/main.html', controller: 'MainCtrl'
+      })
+
+      .when('/about', {
+        templateUrl: 'views/about.html', controller: 'AboutCtrl'
+      })
+
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularDataApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the angularDataApp
+ */
+angular.module('angularDataApp')
+  .controller('LoginCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
