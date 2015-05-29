9b96c2213208520dd3083c632e34c4cc8c51306a
Manu <bionikspoon@gmail.com>
2015-05-28 22:11:56 -0500

    checkins form

--- app/scripts/controllers/checkins.js
+++ app/scripts/controllers/checkins.js
@@ -7,7 +7,27 @@
  * # CheckinsCtrl
  * Controller of the angularDataApp
  */
-angular.module('angularDataApp')
-  .controller('CheckinsCtrl', function ($scope) {
+angular.module('angularDataApp').controller('CheckinsCtrl',
+  function ($scope, $routeParams, $firebaseArray, FIREBASE_URL) {
+
+    $scope.whichMeeting = $routeParams.meetingId;
+    $scope.whichUser = $routeParams.userId;
+
+    var ref = new Firebase(FIREBASE_URL + '/users/' + $scope.whichUser +
+      '/meetings/' + $scope.whichMeeting + '/checkins');
+
+    $scope.addCheckin = function () {
+      var checkinsObj = $firebaseArray(ref);
+      checkinsObj.$add({
+        firstName: $scope.user.firstName,
+        lastName: $scope.user.lastName,
+        email: $scope.user.email,
+        date: Firebase.ServerValue.TIMESTAMP
+      })
+
+        .then(function () {
+
+        });
+    };
 
   });
