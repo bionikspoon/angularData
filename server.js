3deb7e1fdf8de907e74961d63d148b70cd53fba5
Manu <bionikspoon@gmail.com>
2015-05-29 21:59:28 -0500

    added dist

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 'use strict';
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(express.static('dist'));
 
-app.listen(5000);
+app.listen(port);
